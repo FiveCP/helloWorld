6640ae2fb7bf7aca9312e5e00d0ddfa252b2fbcb
wcp <2318263528@qq.com>
2018-07-19 22:53:22 +0800

    test

---
+++ src/helloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloWorld;
+
+public class HelloWorld {
+	public static void mian(String[] args) {
+		int a = 0;
+		if (a == 0) {
+			System.out.print("Hello world!");
+		} else {
+			System.out.println("Hello git!");
+		}
+	}
+}
